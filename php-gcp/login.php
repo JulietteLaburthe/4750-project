c546621730bbd96be26be4311f1fbc8f33ddf9f5
JulietteLaburthe <77637917+JulietteLaburthe@users.noreply.github.com>
2022-03-27 19:12:19 -0400

    Add files via upload

--- php-gcp/login.php
+++ php-gcp/login.php
@@ -1,1 +1,25 @@
-<?php echo "this is the login page testing it is linking properly" ?>
+
+<?php
+require('connect-db.php');
+require('user_functions.php');
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+  if (!empty($_POST['btnAction']) && $_POST['btnAction'] == "Login"){
+    login($_POST['email']);
+   
+  }
+    
+}
+?>
+
+<form name="mainForm" action="login.php" method="post">   
+  <div class="row mb-3 mx-3">
+    Email:
+    <input type="text" class="form-control" name="email"  
+            ?>        
+  </div>  
+  <input type="submit" value="Login" name="btnAction" class="btn btn-dark" 
+        title="Login" />  
+</form>
